2dde8f9a274fb2a6e9d8cd5812b7c14d9f80151c
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 15:39:54 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,6 +6,7 @@ terraform {
     #sas_token            = "$SAS_TOKEN"
     sas_token = "?sv=2022-11-02&ss=b&srt=sco&sp=rwdlactf&se=2024-02-01T21:55:07Z&st=2023-11-07T13:55:07Z&spr=https&sig=6yDxOkmCliag8NoUt44HzyE%2Bkl8lYPrUcr8n2g8fxRE%3D"
     key       = "enagasmodulesg.tfstate"
+    features {}
   }
 
 
