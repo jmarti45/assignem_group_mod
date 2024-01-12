422542a33a6ea5c22e328fa88106038d4c5c262f
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 14:43:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ module "Groups" {
   securitygroup = "az-mg-enagas-wintel"
 
 }
+
+
+
+module "MGroups" {
+  source = "git::https://github.com/jmarti45/assignment_group_orig.git"
+  providers = {
+
+  }
+  managementgroup = "a9ec01ef-74c5-41c9-97c7-94aeefcef2cc"
+
+}
