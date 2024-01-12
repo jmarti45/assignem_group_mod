59aba20f310e5592bc310e1fd07e532e3f57eb2e
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 14:57:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,15 @@ module "Groups" {
 
 }
 
+# assign reader role for team working on Management group 
+resource "azurerm_role_assignment" "management" {
+  scope                = "a9ec01ef-74c5-41c9-97c7-94aeefcef2cc"
+  role_definition_name = "reader"
+  principal_id         = azuread_group.group1.object_id
+}
+
+
+
 /*
 
 module "MGroups" {
