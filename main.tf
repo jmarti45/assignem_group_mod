035ce59f3165b5e98fb5e7303cc784ab43a3063e
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 11:22:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,3 @@ module "Groups" {
   securitygroup = "az-mg-enagas-wintel"
 
 }
-/*
-module "Spoke2" {
-  source = "git::https://github.com/jmarti45/assignem_group_mod.git"
-
-  Subscription = var.subscription
-
-}
-
-#### Test
-*/
