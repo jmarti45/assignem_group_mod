57465ba893ce230074e638c80ef7bffa08a42ddc
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 15:41:12 +0000

    2d3d33

--- Assinment.tf
+++ Assinment.tf
@@ -1,9 +1,7 @@
 
 module "MGroups" {
   source = "git::https://github.com/jmarti45/assignment_group_orig.git"
-  providers = {
 
-  }
   mgroup        = "a9ec01ef-74c5-41c9-97c7-94aeefcef2cc"
   securitygroup = "az-mg-enagas-wintel"
 }
