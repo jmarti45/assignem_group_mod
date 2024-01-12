f1e9ccb594a1ec18a48fd462a0df36b4304f6d55
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 12:14:57 +0000

    s222s2s2

--- main.tf
+++ main.tf
@@ -9,13 +9,3 @@ module "Groups" {
 
 }
 
-/*
-module "assigns" {
-  source = "git::https://github.com/jmarti45/assignment_group_orig.git"
-  providers = {
-
-  }
-  managementgroup = "a9ec01ef-74c5-41c9-97c7-94aeefcef2cc"
-
-}
-*/
