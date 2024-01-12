3d67151c3267706cf9ab85ca64101bc129c399b7
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 15:05:57 +0000

    d33d33d3d

--- main.tf
+++ main.tf
@@ -9,16 +9,7 @@ module "Groups" {
 
 }
 
-# assign reader role for team working on Management group 
-resource "azurerm_role_assignment" "management" {
-  scope                = "a9ec01ef-74c5-41c9-97c7-94aeefcef2cc"
-  role_definition_name = "reader"
-  principal_id         = "add6e66b-9ef0-43f4-ab42-258dbbe25c32"
-}
 
-
-
-/*
 
 module "MGroups" {
   source = "git::https://github.com/jmarti45/assignment_group_orig.git"
@@ -28,4 +19,3 @@ module "MGroups" {
   managementgroup = "a9ec01ef-74c5-41c9-97c7-94aeefcef2cc"
 
 }
-*/
