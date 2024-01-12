65a1e82d4a01eecc973289a1203704d9cfcc40ce
jmarti45 <jmartinez72@hotmail.es>
2024-01-12 12:13:52 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "Groups" {
 
 }
 
-
+/*
 module "assigns" {
   source = "git::https://github.com/jmarti45/assignment_group_orig.git"
   providers = {
@@ -18,3 +18,4 @@ module "assigns" {
   managementgroup = "a9ec01ef-74c5-41c9-97c7-94aeefcef2cc"
 
 }
+*/
